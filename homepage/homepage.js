869029a0f3c7296e37facb867b36f2fe080873f2
jaguillo <jaguillo@student.42.fr>
2016-11-27 19:32:02 +0100

    Draw some graph

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 15:22:05 by jaguillo          #+#    #+#             */
-/*   Updated: 2016/11/27 15:44:46 by jaguillo         ###   ########.fr       */
+/*   Updated: 2016/11/27 19:29:48 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ function http_request(url, post, on_success, on_error)
 }
 
 // string -> int -> (string -> 'a) -> (('a -> unit) -> unit)
-function cached_http_request(url, expire_delta, data_process)
+function Cached_http_request(url, expire_delta, data_process)
 {
 	var CACHE_KEY = "cache::" + url;
 	var EXPIRE_KEY = "cache_expire::" + url;
@@ -99,7 +99,7 @@ function cached_http_request(url, expire_delta, data_process)
 	return (function(callback)
 	{
 		get_from_cache(callback) || get_from_url(callback);
-	})
+	});
 }
 
 /*
@@ -238,29 +238,165 @@ function parse_weather_data(data)
 	data = JSON.parse(data);
 
 	return ({
-		"current": parse_current(data["current_observation"]),
-		"hourly": data["hourly_forecast"].map(parse_hourly),
-		"moon": parse_moon_phase(data["moon_phase"]),
-		"sun": parse_sun_phase(data["sun_phase"])
+		current: parse_current(data["current_observation"]),
+		hourly: data["hourly_forecast"].map(parse_hourly),
+		moon: parse_moon_phase(data["moon_phase"]),
+		sun: parse_sun_phase(data["sun_phase"])
 	});
 }
 
 /*
 ** ========================================================================== **
+** Canvas util
 */
 
-function render(data)
+var Canvas_util = function(canvas)
 {
-	console.log("render", data);
-}
+	var context = canvas.getContext("2d");
+
+	function map_path(data, get_x, get_y)
+	{
+		context.beginPath();
+		context.moveTo(get_x(0, data[0]), get_y(0, data[0]));
+		for (var i = 1; i < data.length; i++)
+			context.lineTo(get_x(i, data[i]), get_y(i, data[i]));
+		context.stroke();
+	}
+
+	function clear()
+	{
+		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	}
+
+	function set(attrs)
+	{
+		for (k in attrs)
+			context[k] = attrs[k];
+	}
+
+	return ({
+		context: context,
+		width: context.canvas.width,
+		height: context.canvas.height,
+		clear: clear,
+		set: set,
+		map_path: map_path
+	});
+};
 
 /*
 ** ========================================================================== **
 */
 
-var weather_request = cached_http_request(
+(function(){
+
+var LOOP_INTERVAL = 60;
+
+var weather_request = Cached_http_request(
 	"http://api.wunderground.com/api/b08b95a1341330dd/astronomy/conditions/hourly/bestfct:1/q/48.8566,2.3522.json",
 	30 * 60,
 	parse_weather_data)
 
-weather_request(render);
+function WeatherGraph(canvas, stroke_style, minmin, maxmax, get_data)
+{
+	var canvas = Canvas_util(canvas);
+
+	var GRAPH_OFFSET_X = 10;
+	var GRAPH_OFFSET_Y = 5;
+	var GRAPH_WIDTH = canvas.width - GRAPH_OFFSET_X - 10;
+	var GRAPH_HEIGHT = canvas.height - GRAPH_OFFSET_Y - 15;
+
+	var STYLES = {
+		"lineWidth": 2,
+		"lineCap": "square",
+		"textAlign": "center",
+		"font": "10px Arial, sans-serif",
+		"strokeStyle": stroke_style
+	};
+
+	// 'a array -> ('a -> number) -> number * number
+	function array_minmax_map(array, f)
+	{
+		var tmp;
+		var min, max;
+
+		tmp = f(array[0]);
+		min = max = tmp;
+		for (var i = 1; i < array.length; i++)
+		{
+			tmp = f(array[i]);
+			if (tmp > max)
+				max = tmp;
+			if (tmp < min)
+				min = tmp;
+		}
+		return ([min, max]);
+	}
+
+	function draw_graph(data)
+	{
+		var [min, max] = array_minmax_map(data, get_data);
+		var [min, max] = [Math.min(minmin, min), Math.max(maxmax, max)];
+
+		function get_x(i, _)
+		{
+			return (GRAPH_WIDTH * i / (data.length - 1) + GRAPH_OFFSET_X);
+		}
+
+		function get_y(_, data)
+		{
+			data = get_data(data);
+			return ((max - data) * GRAPH_HEIGHT / (max - min) + GRAPH_OFFSET_Y);
+		}
+
+		canvas.clear();
+		canvas.set(STYLES);
+		canvas.map_path(data, get_x, get_y);
+
+		for (var i = 0; i < data.length; i++)
+		{
+			canvas.context.fillText(get_data(data[i]).toString(), get_x(i, null), 200);
+		}
+	}
+
+	return (draw_graph);
+}
+
+var temp_graph = WeatherGraph(document.getElementById("forecast-temp"), "red",
+		0, 15, function(data){ return (data.temperature); });
+var rain_graph = WeatherGraph(document.getElementById("forecast-rain"), "blue",
+		0, 30, function(data){ return (data.pop); });
+var wind_graph = WeatherGraph(document.getElementById("forecast-wind"), "gray",
+		5, 20, function(data){ return (data.wind_dir); });
+
+var temp_current = document.getElementById("current-temp");
+var wind_current = document.getElementById("current-wind");
+
+function render(data)
+{
+	var hourly_data = data.hourly.slice(0, 15);
+	temp_graph(hourly_data);
+	rain_graph(hourly_data);
+	wind_graph(hourly_data);
+
+	temp_current.innerText = data.current.temperature;
+	wind_current.innerText = data.current.wind_speed;
+}
+
+function requestTimeout(callback, t)
+{
+	setTimeout(function(){ requestAnimationFrame(callback); }, t);
+}
+
+function render_loop()
+{
+	weather_request(function(data)
+	{
+		render(data);
+		requestTimeout(render_loop, LOOP_INTERVAL * 1000);
+	});
+}
+
+render_loop();
+
+})();
